f77b94e089d714a77100f7ad5a099b6a9f740d27
Lucas Daniel Baruja Torales <lucas.baruja@upa.edu.py>
2025-07-21 20:07:50 -0300

    Commit with tailwindcss working properly

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,14 @@
 import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
-import colors from 'tailwindcss/colors'
+
+
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx,css}',
   ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        gray: colors.gray,
-        blue: colors.blue,
-        slate: colors.slate,
-        background: '#1A1A1A',
-        'light-background': '#2A2A2A',
-        'primary-text': '#FFFFFF',
-        'secondary-text': '#A3A3A3',
-        accent: {
-          DEFAULT: '#00F5A0',
-          light: '#5FFFE0',
-          dark: '#00D68F',
-        },
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  
   plugins: [require('@tailwindcss/typography')],
 }
 export default config 
